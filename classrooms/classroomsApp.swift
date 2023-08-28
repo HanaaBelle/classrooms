2838e430593fbc29314f34410a4c17d90ae9cddf
HanaaBelle <hanaabelhamidi@gmail.com>
2023-08-28 15:41:11 +0200

    Initial Commit

---
+++ classrooms/classroomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  classroomsApp.swift
+//  classrooms
+//
+//  Created by Hanâa Belhamidi on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct classroomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
