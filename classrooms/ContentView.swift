b4d437e23b484629097d219e67099cfc335711a4
HanaaBelle <hanaabelhamidi@gmail.com>
2023-08-28 21:37:33 +0200

    Added button with state toggle & pushed menu to the bottom

--- classrooms/ContentView.swift
+++ classrooms/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Déclarer une var qui est un état "a state"
+    @State private var cliqued = false
     var body: some View {
         
         VStack{
@@ -33,6 +35,45 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
                 .padding()
                 .frame(width: 200.0)
+            
+            // Click Here Button
+            Button(cliqued ? "Return" : "Click Here") {
+                cliqued.toggle()
+            }
+            .buttonStyle(.borderedProminent)
+                .tint(Color.purple)
+                .foregroundColor(Color.white)
+                .padding()
+            
+            // Logo
+            Image("classrooms")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            // Menu
+            Image(systemName: "house")
+                .imageScale(.large)
+                .foregroundColor(.purple)
+            HStack{
+                Text("Home")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.purple)
+                    .padding(.leading, 3.0)
+                Text("Courses")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.purple)
+                    .multilineTextAlignment(.center)
+                    .padding(.leading, 3.0)
+                Text("About us")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.purple)
+                    .multilineTextAlignment(.center)
+                    .padding(.leading, 3.0)
+            }
             
         }
         
